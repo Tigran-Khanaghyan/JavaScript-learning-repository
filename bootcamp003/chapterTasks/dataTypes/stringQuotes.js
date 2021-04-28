3b1e53775dc185c7a8caa67ece9afd922bce589a
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-04-28 17:16:03 +0400

    create: add stringQuotes.js task to folder dataTypes

---
+++ bootcamp003/chapterTasks/dataTypes/stringQuotes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//What is the output of the script?
+
+let name = "Ilya";
+
+//output 1:
+console.log(`hello ${1}`); // ?
+//output name:
+console.log(`hello ${"name"}`); // ?
+//output Ilya:
+console.log(`hello ${name}`); // ?
+
+/*Backtick quotes allow us to embed variables
+ and expressions into a string by wrapping them in ${...}. inside
+ ${...}* we can insert number, string, variable.*/
