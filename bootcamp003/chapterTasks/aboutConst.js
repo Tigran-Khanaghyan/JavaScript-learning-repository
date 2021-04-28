737135413dad754b85f3c1585bd81018b9aa06ea
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-04-28 15:58:21 +0400

    create: two js tasks adding in chapterTasks folder

---
+++ bootcamp003/chapterTasks/aboutConst.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Would it be right to use upper case for birthday? For age? Or even for both?
+
+//The birthday variable must be an upperCase mode, because we know the value of that constant before declaring.
+const BIRTHDAY = "18.04.1982";
+
+//The age variable must be a lowerCase mode, because we don`t know about ade`s value before declaring.
+const age = someCode(birthday);
---
+++ bootcamp003/chapterTasks/givingTheRightName.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//1.Create a variable with the name of our planet. How would you name such a variable?
+let planetEarth;
+
+//2.Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentUser;
