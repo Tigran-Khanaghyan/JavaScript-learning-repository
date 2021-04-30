90724c3a4f336952a1f9972506cc787978759c34
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-04-30 12:51:14 +0400

    create: add printPatern.js task to requiredTasks section

--- bootcamp004/requiredTasks/printPattern.js
+++ bootcamp004/requiredTasks/printPattern.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*Print the following pattern:
 1 
@@ -7,4 +7,16 @@
 4 8 11 13
 5 9 12 14 15 */
 
+//1 2 6 3 7 10 4 8 11 13 5 9 12 14 15
 
+for (let i = 1; i <= 5; i++) {
+  let current = i;
+  let txt = "";
+  let step = 4;
+  for (let k = 0; k < i; k++) {
+    txt += `${current} `;
+    current += step;
+    step--;
+  }
+  console.log(txt);
+}
