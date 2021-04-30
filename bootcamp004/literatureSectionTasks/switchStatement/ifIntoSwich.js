f806e9eec5ff05418648204121e864440011ebde
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-04-30 12:11:42 +0400

    create: add 2 js tasks to switchStatement section

---
+++ bootcamp004/literatureSectionTasks/switchStatement/ifIntoSwich.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*Rewrite the code below using a single switch statement:
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+} */
+
+switch (a) {
+  case 0:
+    alert(0);
+  case 1:
+    alert(1);
+  case 2:
+  case 3:
+    alert("2,3");
+}
---
+++ bootcamp004/literatureSectionTasks/switchStatement/rewriteTheSwich.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/*Write the code using if..else which would correspond to the following switch:
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+} */
+
+if (browser == "Edge") {
+  alert("You've got the Edge!");
+} else if (
+  browser == "Chrome" ||
+  browser == "Firefox" ||
+  browser == "Safari" ||
+  browser == "Opera"
+) {
+  alert("Okay we support these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
