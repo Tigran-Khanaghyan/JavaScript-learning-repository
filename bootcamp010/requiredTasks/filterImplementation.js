ffe373e2ac97c8c824eb6225603cdf5b616aa38c
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-05-12 14:26:05 +0400

    feat: add filterImplementation.js  task into requiredTasks

---
+++ bootcamp010/requiredTasks/filterImplementation.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function filter(array, fn) {
+  let filteredArray = [];
+  for (let index = 0; index < array.length; ++index) {
+    let element = array[index];
+    let isTrue = fn(element, index, array);
+    if (isTrue) {
+      filteredArray.push(element);
+    }
+  }
+  return filteredArray;
+}
+
+let users = [
+  { id: 1, name: "Tigran" },
+  { id: 2, name: "Davit" },
+  { id: 3, name: "Mariam" },
+];
+
+let newArray = filter(users, (item) => item.id == 1);
+console.log(newArray);
