0d871d83d380b2c4fd68f2a1091447688af2b507
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-05-14 15:04:51 +0400

    feat: add headAndTailofArray.js into requiredTasks

---
+++ bootcamp012/requiredTasks/headAndTailOfArray.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/*Write functions that will return head and tail of Array
+
+getHead([1, 2, 3]);
+// => [1]
+getTail([1, 2, 3]);
+// => [2, 3] */
+
+let arr = [1, 2, 3];
+
+function getHead(arr) {
+  let [a] = [...arr];
+  return a;
+}
+
+function getTail(arr) {
+  let [a, ...tail] = [...arr];
+  return tail;
+}
+
+console.log(getHead(arr));
+console.log(getTail(arr));
