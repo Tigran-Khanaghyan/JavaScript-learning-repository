8b91b68b538e95be39a0de7164dc4b9c815d66c1
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-05-18 22:55:27 +0400

    feat: add regex.js task into requiredTasks

---
+++ bootcamp014/requiredTasks/regex.js
@@ -0,0 +1,105 @@
+"use strict";
+
+/*let regex = /abc/
+
+let str = 'abcde'
+
+console.log(str.match(regex)) */
+
+/***************************************************/
+
+/*let str = '	abc123xyz'
+
+let regex = /\d{3}/
+
+console.log(str.match(regex)) */
+
+/***************************************************/
+
+/*let str = '	cat.'
+let regex = /\./
+
+console.log(str.match(regex)) */
+
+/***************************************************/
+
+/*let str = 'Tigran'
+let regex = /[fi]/
+console.log(str.match(regex))  */
+
+/***************************************************/
+
+/*let str = "Tigran";
+let regex = /[^Tigran]/;
+console.log(str.match(regex)); */
+
+/**************************************************/
+
+/*let str = 'Tigran'
+let regex = /[a-r]/
+console.log(str.match(regex)) */
+
+/**************************************************/
+
+/*let str = "Tigrannnnnnnnn";
+let regex = /[n]{4}/;
+console.log(str.match(regex));
+ */
+
+/*************************************************/
+
+/*let str = "Tigran";
+let regex = /g+/;
+console.log(str.match(regex)); */
+
+/**************************************************/
+
+/*let str = 'Tigran'
+let regex = /Tigran9?/
+console.log(str.match(regex)) */
+
+/**************************************************/
+
+/*let str = "Tig ran";
+let regex = /\s+/;
+console.log(str.match(regex)); */
+
+/***************************************************/
+
+/*let str = "Tigran Khanaghyan";
+let regexEnd = /Khanaghyan$/;
+let regexBegin = /^Tigran/;
+console.log(str.match(regexEnd));
+console.log(str.match(regexBegin)); */
+
+/****************************************************/
+
+/*let str = "Tigran.Khanaghyan";
+let str1 = "Davit.Khanaghyan";
+let regex = /(.*)\.Khanaghyan/;
+console.log(str.match(regex));
+console.log(str1.match(regex)); */
+
+/****************************************************/
+
+/*let str = 'Tigran khanaghyan'
+let regex = /(([A-Za-z]{6})\s(\w*))/
+console.log(str.match(regex)) */
+
+/****************************************************/
+
+/*let num = '0xffffff'
+let regex = /(0)x(\w{3,6})/
+console.log(num.match(regex)) */
+
+/*****************************************************/
+
+/*let str1 = "Tigran Khanaghyan";
+let str2 = "Davit Khanaghyan";
+let regex = /(Tigran|Davit)/;
+console.log(str1.match(regex));
+console.log(str2.match(regex)); */
+
+/******************************************************/
+
+
