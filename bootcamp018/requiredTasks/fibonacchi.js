b731c36d0268a82e34891ac05ff264cbdd6a248d
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-06-01 16:21:03 +0400

    feat: add fibonacchi.js task into requiredTasks

--- bootcamp018/requiredTasks/fibonacchi.js
+++ bootcamp018/requiredTasks/fibonacchi.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
-function fibonacchi(n){
-    if(n === 0){
-        return 0
-    }
-    if(n === 1){
-        return 1
-    }
-    return fibonacchi(n -1) + fibonacchi(n - 2)
+function fibonacchi(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return fibonacchi(n - 1) + fibonacchi(n - 2);
 }
 
-console.log(fibonacchi(100))
+console.log(fibonacchi(5));
