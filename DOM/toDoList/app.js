7b9c8c2c99ea6c4e4c12ad252abfa84cbeca1049
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-06-19 19:07:07 +0400

    docs(todoList): add todo list task

--- DOM/toDoList/app.js
+++ DOM/toDoList/app.js
@@ -4,6 +4,7 @@ const addItemBtn = document.querySelector(".input-button");
 let inputField = document.querySelector(".input-field");
 const todoItem = document.querySelector(".todo-item");
 let list = document.querySelector(".input-content");
+const clearButton = document.querySelector(".clear-button");
 let items = [];
 let inputFieldValue = "";
 let itemsQuantity = 0;
@@ -27,10 +28,20 @@ addItemBtn.addEventListener("click", () => {
   inputFieldValue = "";
 });
 
+clearButton.addEventListener("click", function clear() {
+  let toDoes = document.querySelectorAll(".todo-item");
+  toDoes.forEach((item) => {
+    if (item.classList.length === 2) {
+      item.remove();
+      items.length = 0;
+      itemsQuantity = 0;
+    }
+  });
+});
+
 function addEventOnCheckIcon(array, id) {
   array.forEach((item) => {
     if (item.id === id) {
-      let text = item.newItem.childNodes[1];
       for (let i = 0; i < item.newItem.childNodes.length; ++i) {
         if (item.newItem.childNodes[i].className === "icons") {
           let checkIcon =
@@ -39,8 +50,39 @@ function addEventOnCheckIcon(array, id) {
           let closeIcon =
             item.newItem.childNodes[i].querySelector(".close-icon");
           checkIcon.addEventListener("click", () => {
-            text.style.color = "green";
-          })
+            let classes = item.newItem.classList;
+            let p = document.querySelector(`.${classes[1]} p`);
+            p.style.color = "green";
+          });
+          editIcon.addEventListener("click", () => {
+            let text = item.newItem.childNodes[1].textContent;
+            let dynamicInput = document.createElement("input");
+            dynamicInput.setAttribute("type", "text");
+            dynamicInput.setAttribute("class", "dynamic-input");
+            let p = item.newItem.childNodes[1];
+            p.parentNode.replaceChild(dynamicInput, p);
+            dynamicInput.value = text;
+            let dynamicInputValue = "";
+            dynamicInput.addEventListener("input", (event) => {
+              dynamicInputValue = event.target.value;
+            });
+            document.body.addEventListener("mousedown", function addP(event) {
+              if (event.target !== dynamicInput) {
+                let p = document.createElement("p");
+                if (!dynamicInputValue) {
+                  p.textContent = text;
+                } else {
+                  p.textContent = dynamicInputValue;
+                }
+                dynamicInput.parentNode.replaceChild(p, dynamicInput);
+                document.body.removeEventListener("mousedown", addP);
+              }
+            });
+          });
+          closeIcon.addEventListener("click", () => {
+            item.newItem.remove();
+            --itemsQuantity;
+          });
         }
       }
     }
